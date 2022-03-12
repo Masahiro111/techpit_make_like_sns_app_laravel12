f8e65108e3f392ad1b8b8a23f261938e62f52956
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-03-12 19:49:02 +0900

    finished 1

--- database/migrations/2022_03_12_104114_create_articles_table.php
+++ database/migrations/2022_03_12_104114_create_articles_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('body');
+            // $table->bigInteger('user_id');
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 };
