4a4977c63a1bd34bbb3288bfb0472e3cba11ffa5
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-03-14 19:07:21 +0900

    5-2

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,14 +1,14 @@
 @csrf
 <div class="md-form">
     <label>タイトル</label>
-    <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+    <input type="text" name="title" class="form-control" required value="{{ old('title', $article->title) }}">
     @error('title')
     <small class="text-red-500">{{ $message }}</small>
     @enderror
 </div>
 <div class="form-group">
     <label></label>
-    <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') }}</textarea>
+    <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body', $article->body) }}</textarea>
     @error('body')
     <small class="text-red-500">{{ $message }}</small>
     @enderror
